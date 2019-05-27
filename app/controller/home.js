0f5c7e3f608286ff01ea83d3e3756cb0ae663609
zxw <you@example.com>
2019-05-27 17:15:11 +0800

    test

--- app/controller/home.js
+++ app/controller/home.js
@@ -2,7 +2,7 @@ const Controller = require('egg').Controller
 
 class HomeController extends Controller {
   async index () {
-    this.ctx.body = 'Hello World'
+    this.ctx.body = 'Hello World!'
   }
 }
 
