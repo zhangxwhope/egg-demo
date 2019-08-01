a047e293a0b41b5180be0e7c7a5ab5a93e155ddd
zxw <you@example.com>
2019-08-01 17:53:58 +0800

    egg

--- config/config.default.js
+++ config/config.default.js
@@ -13,3 +13,15 @@ exports.news = {
   pageSize: 5,
   serverUrl: 'https://hacker-news.firebaseio.com/v0',
 }
+
+// add middleware robot
+exports.middleware = [
+  'robot'
+];
+
+// robot's configurations
+exports.robot = {
+  ua: [
+    /Baiduspider/i,
+  ]
+};
